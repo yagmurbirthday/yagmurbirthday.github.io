d8a77117ce25240238149ca37c9f13586cf6f7ba
James Platt <James.Platt@Neptune.com>
2020-07-11 20:59:37 +0100

    add to repo

---
+++ script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){   
+    getDankMeme();
+});
+
+var HttpClient = function() {
+    this.get = function(aUrl, aCallback) {
+        var anHttpRequest = new XMLHttpRequest();
+        anHttpRequest.onreadystatechange = function() { 
+            if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+                aCallback(anHttpRequest.responseText);
+        }
+
+        anHttpRequest.open( "GET", aUrl, true );            
+        anHttpRequest.send( null );
+    }
+}
+
+function getDankMeme(){
+    var imgElement = document.getElementById("dankMemeImg");
+    var labelElement = document.getElementById("dankMemeLabel");
+
+    var uri = "https://api.reddit.com/r/dankmemes";
+
+    var client = new HttpClient();
+    client.get(uri, function(response) {
+        var memes = JSON.parse(response).data.children;
+        var memeIndex = Math.ceil(Math.random() * memes.length);
+        var selMeme = memes[memeIndex];
+
+        var imgUrl = selMeme.data.preview.images[0].source.url;
+        imgUrl = imgUrl.replace("amp;s", "s");
+
+        labelElement.innerHTML = selMeme.data.title + " - " + selMeme.data.name;
+        imgElement.src = imgUrl;
+    });
+
+}
+
+marqueeInit({
+	uniqueid: 'mycrawler',
+	style: {
+		'padding': '2px',
+		'width': '100%',
+		'height': '180px'
+	},
+	inc: 5, //speed - pixel increment for each iteration of this marquee's movement
+	mouse: 'cursor driven', //mouseover behavior ('pause' 'cursor driven' or false)
+	moveatleast: 2,
+	neutral: 150,
+	savedirection: true,
+	random: true
+});
+
+marqueeInit({
+	uniqueid: 'mycrawler2',
+	style: {
+		'padding': '2px',
+		'width': '100%',
+		'height': '180px'
+	},
+	inc: 5, //speed - pixel increment for each iteration of this marquee's movement
+	mouse: 'cursor driven', //mouseover behavior ('pause' 'cursor driven' or false)
+	moveatleast: 2,
+	neutral: 150,
+	savedirection: true,
+	random: true
+});
